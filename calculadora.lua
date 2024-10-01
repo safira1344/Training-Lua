c9a4447b9c899e910faf275a1bba3305c9faaf64
Fernanda Mirely <safiraaabarbosaaa@gmail.com>
2024-09-30 23:01:25 -0300

    add: calculadora funções simples

---
+++ calculadora.lua
@@ -0,0 +1,19 @@
+function soma(a, b)
+  return a + b
+end
+
+function subtracao(a, b)
+  return a - b
+end
+
+function multipliicao(a, b)
+  return a * b
+end
+
+function divisao(a,b)
+    if b == 0 then
+        print("Divisão por zero")
+    end
+    return a/b
+end
+
